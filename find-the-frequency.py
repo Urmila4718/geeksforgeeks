d21c847837b0631983118301d553f9f3f8a6bb22
Urmila4718 <79640277+Urmila4718@users.noreply.github.com>
2024-04-26 14:06:39 +0530

    Create find-the-frequency.py

---
+++ find-the-frequency.py
@@ -0,0 +1,15 @@
+def findFrequency (arr, n, x):
+    count = 0
+    for i in range(n):
+        if (x==arr[i]):
+            count+=1
+    return count    
+
+
+def findFrequency (arr, n, x):
+    count = 0
+    for i in range(n):
+        if (x==arr[i]):
+            count+=1
+    return count    
+    # Your Code Here
